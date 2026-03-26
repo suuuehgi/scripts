c585b0cca3f74f1f97280dc9d977e38c31264aa3
suuuehgi <7861774+suuuehgi@users.noreply.github.com>
2026-03-26 17:11:14 +0100

    removed code for what's already being done by the paste script.

--- headlinify-highlight.bash
+++ headlinify-highlight.bash
@@ -7,14 +7,9 @@ text=$(wl-paste --primary 2>/dev/null) || { echo "Nothing selected" >&2; exit 1;
 # Transform
 result=$(python3 ~/.local/bin/headlinify "$text")
 
-# Write to clipboard and paste
-printf %s "$result" | wl-copy --type 'text/plain;charset=utf-8'
-
-# wait until pasting to clipboard succeeded (max ~500ms)
-for _ in {1..25}; do
-    wl-paste --no-newline 2>/dev/null | grep -qF "$result" && break
-    sleep 0.02
-done
+# Time to release keys
+# (If this script is being bound to a key combination - otherwise they mix up with the key presses emulated in the next line.)
+sleep .3
 
 # Alternative: ydotool key 29:1 47:1 47:0 29:0   # Ctrl+V
 # (pasty is a symlink to anonymize.py)
