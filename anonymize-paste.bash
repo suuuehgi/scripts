22fbb4593a47b26665cb8cdad22af89f9ebfaf5f
suuuehgi <7861774+suuuehgi@users.noreply.github.com>
2026-03-26 17:10:35 +0100

    Already surround content in backticks.

--- anonymize-paste.bash
+++ anonymize-paste.bash
@@ -6,10 +6,13 @@ text=$(wl-paste 2>/dev/null) || { echo "Nothing copied" >&2; exit 1; }
 
 # Anonymize
 # (anonymize is a symlink to anonymize.py)
-result=$(echo "$text" | ~/.local/bin/anonymize)
+result="$(echo "$text" | ~/.local/bin/anonymize)"
 
-# Time to release the keyboard shortcut
-# (if this script is being bound to a key combination)
+# Enclose in ``` (markdown code formatters)
+result=$(printf '```\n%s\n```' "$result")
+
+# Time to release keys
+# (If this script is being bound to a key combination - otherwise they mix up with the key presses emulated in the next line.)
 sleep .3
 
 # Alternative: ydotool key 29:1 47:1 47:0 29:0   # Ctrl+V
