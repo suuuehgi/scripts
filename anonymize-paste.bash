55f88d8d05b87ad70bb14ffcb7a41586f07bdca2
suuuehgi <7861774+suuuehgi@users.noreply.github.com>
2026-03-26 16:54:11 +0100

    removed left over from the previous commit.
    added a short delay for the human to release the keys. Otherwise the keyboard shortcut mixes up with the keys ydotool presses (paste.bash).

--- anonymize-paste.bash
+++ anonymize-paste.bash
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# PRIMARY selection = whatever is currently highlighted (no Ctrl+C needed)
+# Get clipboard content
 text=$(wl-paste 2>/dev/null) || { echo "Nothing copied" >&2; exit 1; }
 
-# Transform
+# Anonymize
 # (anonymize is a symlink to anonymize.py)
 result=$(echo "$text" | ~/.local/bin/anonymize)
 
-# Write to clipboard and paste
-printf %s "$result" | wl-copy --type 'text/plain;charset=utf-8'
+# Time to release the keyboard shortcut
+# (if this script is being bound to a key combination)
+sleep .3
 
 # Alternative: ydotool key 29:1 47:1 47:0 29:0   # Ctrl+V
 # (pasty is a symlink to paste.bash)
